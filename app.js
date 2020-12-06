79ce9708fa9df8c615c7df1a8f1f5a8888d4a81f
Ankit-Nag <ankitknag@gmail.com>
2020-12-06 10:14:28 +0530

    v1 complete
    Added all functionality and styles.
    Note: Dovahzul API is down, used Thuum API instead

---
+++ app.js
@@ -0,0 +1,28 @@
+//setup the inputs and outputs
+var btnTranslate = document.querySelector("#btnTranslate");
+var txtInput = document.querySelector("#input-box");
+var outputDiv = document.querySelector("#output-box");
+//server URL to minion API
+var serverURL = "https://api.funtranslations.com/translate/thuum.json"
+// Dovahzul API down, currently using Thuum API
+// Original API - https://api.funtranslations.com/translate/dovahzul.json
+//prepare server URL for querying [load]
+function getTranslationURL(text) {
+    return serverURL + "?" + "text=" + text
+}
+//error handling
+function errorHandler(error){
+    console.log("Error occured ",error)
+}
+//click event
+function clickEventHandler() {
+    var inputText = txtInput.value;
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {outputDiv.innerText=json.contents.translated
+        })
+        // 
+        .catch(errorHandler)
+};
+
+btnTranslate.addEventListener("click", clickEventHandler)
